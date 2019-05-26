fe30dbf1888438cfc46cc7589ea908fcef4af26c
Lincoln Stoll <lincoln.stoll@gmail.com>
2019-05-25 21:48:33 -0500

    make the app

---
+++ README.md
@@ -0,0 +1,3 @@
+# alertr - simple external monitoring
+
+Really basic tool to run on Heroku to watch an alertmanager/prometheus install
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"golang.org/x/xerrors"
+)
+
+var httpcli = &http.Client{
+	Timeout: time.Second * 30,
+}
+
+func main() {
+	var (
+		webhook   = flag.String("webhook", ("SLACK_WEBHOOK_URL"), "Slack webhook URL")
+		channel   = flag.String("channel", os.Getenv("SLACK_CHANNEL"), "Channel to notify")
+		mention   = flag.String("mention", os.Getenv("MENTION"), "Who to mention in the message")
+		endpoints = flag.String("endpoints", os.Getenv("ENDPOINTS"), "URLs to monitor")
+	)
+	flag.Parse()
+
+	if *webhook == "" || *endpoints == "" {
+		flag.PrintDefaults()
+		log.Fatal("webhook and endpoints are required flags")
+	}
+
+	for _, ep := range strings.Split(*endpoints, ",") {
+		log.Printf("Checking %s", ep)
+		resp, err := httpcli.Get(ep)
+		if err != nil {
+			log.Printf("Error fetching %s: %v", ep, err)
+			if err := slackNotify(*webhook, *channel, fmt.Sprintf("%s Error fetching %s: %v", *mention, ep, err)); err != nil {
+				log.Printf("Error posting webhook: %v", err)
+				continue
+			}
+		}
+		if resp.StatusCode >= 400 {
+			log.Printf("Error fetching %s: got status %d", ep, resp.StatusCode)
+			if err := slackNotify(*webhook, *channel, fmt.Sprintf("%s Error fetching %s, got status %d", *mention, ep, resp.StatusCode)); err != nil {
+				log.Printf("Error posting webhook: %v", err)
+				continue
+			}
+		}
+	}
+}
+
+type slackPayload struct {
+	IconEmoji string `json:"icon_emoji,omitempty"`
+	Channel   string `json:"channel,omitempty"`
+	Text      string `json:"text,omitempty"`
+	LinkNames bool   `json:link_names,omitempty`
+}
+
+func slackNotify(webhookUrl, channel, msg string) error {
+	pl := slackPayload{
+		Text:      msg,
+		LinkNames: true,
+	}
+
+	slackBody, err := json.Marshal(&pl)
+	if err != nil {
+		return xerrors.Errorf("couldn't marshal message: %w", err)
+	}
+	req, err := http.NewRequest(http.MethodPost, webhookUrl, bytes.NewBuffer(slackBody))
+	if err != nil {
+		return xerrors.Errorf("failed creating HTTP request: %w", err)
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := httpcli.Do(req)
+	if err != nil {
+		return xerrors.Errorf("failed posting webhook: %w", err)
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	if buf.String() != "ok" {
+		return xerrors.New("Non-ok response returned from Slack")
+	}
+
+	return nil
+}
